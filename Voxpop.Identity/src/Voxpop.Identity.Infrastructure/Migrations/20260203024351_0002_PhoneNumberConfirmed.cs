f6526974145c2ad726733b666e1809c08585ffc4
Guilherme Osaka <guilhermeosaka@gmail.com>
2026-02-03 14:08:43 -0300

    IDTY-13 Updates migration

---
+++ Voxpop.Identity/src/Voxpop.Identity.Infrastructure/Migrations/20260203024351_0002_PhoneNumberConfirmed.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Voxpop.Identity.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class _0002_PhoneNumberConfirmed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsService",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "VerificationCode",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "VerificationCodeExpiresAt",
+                table: "AspNetUsers");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsService",
+                table: "AspNetUsers",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "VerificationCode",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "VerificationCodeExpiresAt",
+                table: "AspNetUsers",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+    }
+}
